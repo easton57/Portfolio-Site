928afecf8d5833eced4916e4c26044466a00e6f2
Easton Seidel <eseidel1357@gmail.com>
2024-10-11 08:35:16 -0700

    Created buttons, a side nav bar, etc.

---
+++ JS/home.js
@@ -0,0 +1,6 @@
+document.querySelector("#tech-button").addEventListener("click", function () {
+    window.location.href = "tech_profile.html";
+});
+document.querySelector("#three-d-button").addEventListener("click", function () {
+    window.location.href = "3d_profile.html";
+});
